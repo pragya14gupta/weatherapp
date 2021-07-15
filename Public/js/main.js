defee7893b04b0ff68013db90189a7a3b83cb9ae
pragya <guptapragya1410@gmail.com>
2021-07-15 16:25:55 +0530

    hii

--- Public/js/main.js
+++ Public/js/main.js
@@ -18,7 +18,8 @@ const getInfo = async (event) => {
 
     } else {
         try {
-            let url = `http://api.openweathermap.org/data/2.5/weather?q=${cityval}&units=metric&appid=b5f182c118597731f3d080b40237f84b`;
+            
+            let url = `https://api.openweathermap.org/data/2.5/weather?q=${cityval}&units=metric&appid=b5f182c118597731f3d080b40237f84b`;
             const response = await fetch(url);
             const data = await response.json();
             const arrData = [data];
